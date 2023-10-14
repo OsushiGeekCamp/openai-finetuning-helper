d5714ac22edb4ef41d954d06b71e6d12be79fd5d
niimurank <112260913+niimurank@users.noreply.github.com>
2023-10-15 00:00:48 +0900

    add InputApiKey component for OpenAI API key input

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,11 @@
+export const saveApiKey = (value: string): void => {
+  sessionStorage.setItem('apiKey', value);
+};
+
+export const getApiKey = (): string | null => {
+  return sessionStorage.getItem('apiKey');
+};
+
 export const fetchFiles = async (apiKey: string) => {
   const response = await fetch('https://api.openai.com/v1/files', {
     headers: {
