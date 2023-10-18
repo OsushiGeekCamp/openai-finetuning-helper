d5f750f0612d99bada717996a81f30c195f16748
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-10-18 11:33:39 +0900

    add SpecificFileEditor

---
+++ src/app/editor/[id]/page.tsx
@@ -0,0 +1,7 @@
+import SpecificFileEditorPageContainer from './containers/specific-file-editor-page';
+
+const SpecificFileEditor = () => {
+  return <SpecificFileEditorPageContainer />;
+};
+
+export default SpecificFileEditor;
