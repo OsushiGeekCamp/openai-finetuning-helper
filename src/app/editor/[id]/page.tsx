75dfd1a5184886131a169c06a83c2cb36af7cf9e
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-10-18 13:33:02 +0900

    Add generateStaticParams function

--- src/app/editor/[id]/page.tsx
+++ src/app/editor/[id]/page.tsx
@@ -1,5 +1,7 @@
 import SpecificFileEditorPageContainer from './containers/specific-file-editor-page';
 
+export const generateStaticParams = () => [];
+
 const SpecificFileEditor = () => {
   return <SpecificFileEditorPageContainer />;
 };
