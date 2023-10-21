704cd2e2f7f8ae662c618f8cb48050ba2db13915
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-10-21 13:10:12 +0900

    remove /src/app/eidtor/[id]

--- src/app/editor/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import SpecificFileEditorPageContainer from './containers/specific-file-editor-page';
-
-export const generateStaticParams = () => [];
-
-const SpecificFileEditor = () => {
-  return <SpecificFileEditorPageContainer />;
-};
-
-export default SpecificFileEditor;
