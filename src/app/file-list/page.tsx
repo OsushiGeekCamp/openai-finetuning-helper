6550eed8e012874164e7be0274e5efad91a2335e
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-01-05 23:55:08 +0900

    Revert "Refactor file-list page styling"
    This reverts commit ca4011313ad3bcae872b721c277ba03339c64121.

--- src/app/file-list/page.tsx
+++ src/app/file-list/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import localFont from 'next/font/local';
 import Link from 'next/link';
 
 import { fetchFiles } from '@/utils/openai';
 import Loading from '@/components/loading';
 import { getApiKey } from '@/utils/openai';
+
+import style from './styles.module.css';
 
 type File = {
   id: string;
@@ -154,7 +157,7 @@ const FileList = () => {
               {sortableColumns.map((header, index) => (
                 <th
                   key={index}
-                  className='sticky py-3 px-6 text-left font-medium cursor-pointer transition duration-200 rounded-lg border border-gray-900 dark:bg-gray-700 border-gray-500 border-opacity-50 hover:bg-opacity-70 dark:bg-gray-700 dark:hover:bg-gray-800'
+                  className='sticky top-0 py-3 px-6 text-left font-medium cursor-pointer transition duration-200 bg-gray-500 bg-opacity-50 hover:bg-opacity-70 dark:bg-gray-700 dark:hover:bg-gray-800'
                   onClick={() => sortData(header)}
                 >
                   <div className='flex items-center justify-between'>
@@ -169,7 +172,7 @@ const FileList = () => {
                   </div>
                 </th>
               ))}
-              <th className='sticky py-3 px-6 text-left font-medium flex justify-center bg-gray-500 bg-opacity-50'>
+              <th className='sticky top-0 py-3 px-6 text-left font-medium flex justify-center bg-gray-500 bg-opacity-50'>
                 Quick Start
               </th>
             </tr>
